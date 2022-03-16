4cfd613d1b45eb6586db70028f63320b845da211
pikesiting <990830383@qq.com>
2022-03-16 22:43:26 +0800

    22

---
+++ test3-21-2/test3-21-2/test.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+int main()
+{
+	//double a = 5 / 2;
+	//printf("%d\n", a);
+	//printf("%lf\n", a);
+	int a =-2;
+	int b = 3;
+	printf("%d\n", a|b);
+	return 0;
+}
---
+++ test3-22/test3-22/test.c
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+int main()
+{
+	float a = 30;
+	float b = 2;
+	float c = a/b;
+	printf("%lf\n", c);
+	return 0;
+}
+//int main()
+//{
+//	int a = 10, b = 3;
+//	//int c = (a > b, a = b + 10, a, b = a + 1);
+//	//printf("%d\n", c);
+//	printf("%d\n", a%b*a);
+//	printf("%d\n", b);
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 10, b = 20;
+//	int max = a > b ? a : b;
+//	printf("%d\n", max);
+//	return 0;
+//}
+//int main()
+//{
+//	short s = 0;
+//	int a = 10;
+//
+//	printf("%d\n", sizeof(s=a+5));
+//	printf("%d\n", s);
+//	return 0;
+//}
